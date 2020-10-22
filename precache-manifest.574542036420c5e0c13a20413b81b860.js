e96235dab1e24e0100c044c14bbd9f6c38a8a7b4
freak1ngawesome <qwerty4985@mail.ru>
2020-10-22 12:12:05 +0300

    Updates

---
+++ precache-manifest.574542036420c5e0c13a20413b81b860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21b1e5e2dddb40b27153cfe222c5dcc",
+    "url": "/redux-project/index.html"
+  },
+  {
+    "revision": "62f4957dfef687bddccf",
+    "url": "/redux-project/static/css/main.2845cdde.chunk.css"
+  },
+  {
+    "revision": "c4f13409a5ec072e23c3",
+    "url": "/redux-project/static/js/2.cef0fd05.chunk.js"
+  },
+  {
+    "revision": "62f4957dfef687bddccf",
+    "url": "/redux-project/static/js/main.bee30e0c.chunk.js"
+  },
+  {
+    "revision": "1d69f75c60415276664a",
+    "url": "/redux-project/static/js/runtime~main.3ebf95e2.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/redux-project/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/redux-project/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
